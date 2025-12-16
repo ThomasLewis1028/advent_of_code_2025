44f487b49864fb950b8a736356f1fe2b31ce9a91
thomas <thomas.a.lewis@proton.me>
2025-12-15 22:02:46 -0700

    Finished Day 3

--- advent_of_code_2025/Day3.cs
+++ advent_of_code_2025/Day3.cs
@@ -10,29 +10,28 @@ public static class Day3
 
         var input = File.ReadAllText("input/day3.txt").Trim();
 
-        List<int> banks =
+        List<string> banks =
             input
                 .Split("\n")
-                .Select(x => { return (int.Parse(x)); })
                 .ToList();
 
         Console.WriteLine("\tPart 1: " + Part1(banks));
-        // Console.WriteLine("\tPart 2: " + Part2(ranges));
+        Console.WriteLine("\tPart 2: " + Part2(banks));
     }
 
-    private static Int64 Part1(List<int> banks)
+    private static Int128 Part1(List<string> banks)
     {
-        int total = 0;
+        Int128 total = 0;
 
         foreach (var bank in banks)
         {
-            List<int> batteries = bank.ToString().Select(b => b - '0').ToList();
+            List<int> batteries = bank.Select(b => b - '0').ToList();
 
             var first = 0;
             var second = 0;
             var firstIndex = 0;
-                
-            for(int i = 0; i < batteries.Count-1; i++)
+
+            for (int i = 0; i < batteries.Count - 1; i++)
             {
                 if (batteries[i] > first)
                 {
@@ -40,11 +39,51 @@ public static class Day3
                     firstIndex = i;
                 }
             }
-            
-            
-            
+
+            for (int i = firstIndex + 1; i < batteries.Count; i++)
+            {
+                if (batteries[i] > second)
+                {
+                    second = batteries[i];
+                }
+            }
+
+            total += Int128.Parse(first + second.ToString());
         }
-        
+
+        return total;
+    }
+
+    private static Int128 Part2(List<string> banks)
+    {
+        Int128 total = 0;
+        List<Int128> matches = new List<Int128>();
+
+        foreach (var bank in banks)
+        {
+            List<int> batteries = bank.Select(b => b - '0').ToList();
+
+            List<int> batArray = new List<int>();
+            var index = 0;
+
+            for (int i = 0; i < 12; i++)
+            {
+                batArray.Add(0);
+
+                for (int j = index; j < batteries.Count - 12 + batArray.Count; j++)
+                {
+                    if (batteries[j] > batArray[i])
+                    {
+                        batArray[i] = batteries[j];
+
+                        index = j+1;
+                    }
+                }
+            }
+
+            total += Int128.Parse(batArray.Aggregate("", (a, b) => a + b));
+            matches.Add(Int128.Parse(batArray.Aggregate("", (a, b) => a + b)));
+        }
 
         return total;
     }
